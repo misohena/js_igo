74160beec2924361a9261b221074bf7e96a27cce
AKIYAMA Kouhei <misohena@gmail.com>
2020-05-13 09:45:25 +0900

    Add file selector to SGF import dialog

--- igo_view.js
+++ igo_view.js
@@ -1673,14 +1673,71 @@ class GameView{
         update();
     }
     importSGF(){
+        const CHAR_ENCODINGS = [
+            "UTF-8",
+            "Shift_JIS","EUC-JP","ISO-2022-JP","Big5","EUC-KR","GBK","gb18030",
+            "IBM866","ISO-8859-2","ISO-8859-3","ISO-8859-4","ISO-8859-5",
+            "ISO-8859-6","ISO-8859-7","ISO-8859-8","ISO-8859-8-I","ISO-8859-10",
+            "ISO-8859-13","ISO-8859-14","ISO-8859-15","ISO-8859-16",
+            "KOI8-R","KOI8-U","macintosh","windows-874","windows-1250",
+            "windows-1251","windows-1252","windows-1253","windows-1254",
+            "windows-1255","windows-1256","windows-1257","windows-1258",
+            "x-mac-cyrillic","UTF-16BE","UTF-16LE",
+        ];
+        let fileInput, encodingSelect;
         const {dialog, textarea} = createTextDialog(
-            "Import SGF",
+            [
+                createElement("div", {}, "SGFインポート"),
+                createElement("div", {}, "SGFをペーストするかファイルを選択/ドロップしてください。")
+            ],
             "",
-            [],
+            [
+                createElement("div", {class:"igo-control-bar"}, [
+                    fileInput = createElement("input", {type:"file"}),
+                    createElement("label", {}, [
+                        encodingSelect = createElement(
+                            "select", {},
+                            CHAR_ENCODINGS.map(enc=>createElement("option", {value:enc}, enc)))
+                    ])
+                ]),
+            ],
             ()=>{
-                const game = Game.fromSGF(textarea.value);
-                this.resetGame(game);
+                try{
+                    const game = Game.fromSGF(textarea.value);
+                    this.resetGame(game);
+                }
+                catch(e){
+                    alert("Error: " + e.message);
+                }
             });
+
+        // input file
+        function loadText(file){
+            if(file){
+                const reader = new FileReader();
+                reader.onload = ()=>{
+                    textarea.value = reader.result;
+                    fileInput.value = "";
+                };
+                reader.readAsText(file, encodingSelect.value);
+            }
+        }
+        fileInput.addEventListener("change", ev=>{
+            loadText(fileInput.files[0]);
+        }, false);
+        textarea.addEventListener("dragenter", ev=>{
+            ev.preventDefault();
+            ev.stopPropagation();
+        }, false);
+        textarea.addEventListener("dragover", ev=>{
+            ev.preventDefault();
+            ev.stopPropagation();
+        }, false);
+        textarea.addEventListener("drop", ev=>{
+            ev.preventDefault();
+            ev.stopPropagation();
+            loadText(ev.dataTransfer.files[0]);
+        }, false);
     }
 
     openGameInfo(){
